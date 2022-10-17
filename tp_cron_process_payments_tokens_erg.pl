7c43892b3cf73d1a41e2dcac085c9524c6bae083
Andre Hafner <7738774+andrehafner@users.noreply.github.com>
2022-10-17 09:17:37 -0400

    Update tp_cron_process_payments_tokens_erg.pl
    cleaned up a little

--- tp_cron_process_payments_tokens_erg.pl
+++ tp_cron_process_payments_tokens_erg.pl
@@ -52,94 +52,90 @@ my $int = "0";
 #fire up the loop
 foreach (@files){
 
-#set a var so we can delete the file when finished
-my $del_file = @files[$int];
+  #set a var so we can delete the file when finished
+  my $del_file = @files[$int];
 
-#set command, dir, filename for system call
-my $pyfile = "python3 @files[$int]";
+  #set command, dir, filename for system call
+  my $pyfile = "python3 @files[$int]";
 
-#system call to run python script
-my @pay = qx($pyfile);
+  #system call to run python script
+  my @pay = qx($pyfile);
 
-unlink($del_file);
+  unlink($del_file);
 
-#remove the dir from string
-@files[$int] =~ s/\/usr\/lib\/payments_payroll\///s;
+  #remove the dir from string
+  @files[$int] =~ s/\/usr\/lib\/payments_payroll\///s;
 
-#look at filename for the pay cycle
-if (@files[$int] =~ m/_12erg/){
-$pay_frequency = '12';
-}
-#look at filename for the pay cycle
-elsif (@files[$int] =~ m/_24erg/){
-$pay_frequency = '24';
-}
-#look at filename for the pay cycle
-elsif (@files[$int] =~ m/_52erg/){
-$pay_frequency = '52';
-}
+  #look at filename for the pay cycle
+  if (@files[$int] =~ m/_12erg/){
+    $pay_frequency = '12';
+  }
+    #look at filename for the pay cycle
+    elsif (@files[$int] =~ m/_24erg/){
+      $pay_frequency = '24';
+    }
+    #look at filename for the pay cycle
+    elsif (@files[$int] =~ m/_52erg/){
+      $pay_frequency = '52';
+    }
 
-#feedback when run in the console
-print "(@files[$int] \n\n";
-
-
-#wipe everything after the _ in the filename, this gets us the pure wallet number of file
-@files[$int] =~ s/_.*//s;
-
-#remove trailing and pre spaces
-@files[$int] =~ s/^\s+//;
-@files[$int] =~ s/\s+$//;
-
-#lol, i should rename this, getting the var ito another var
-my $tessst = @files[$int];
-
-#don't need to do this again, can remove
-$tessst =~ s/^\s+//;
-$tessst =~ s/\s+$//;
-
-#prob can just declare this var above and remove the end from it instead of this far down
-#this is where the TX will now be since the python script wrote it here
-# DON'T ask me why the python script won't do a proper STDOUT like the create wallet python did, this is the only way i an get the tx for now
-$tessst = "/usr/lib/cgi-bin/tosipayroll/erg_$tessst";
-
-#this gets the tx from the file
-open $fh, '<', "$tessst" or die "Can't open file $!";
-$tx_address = do { local $/; <$fh> };
-
-#clean up before and after spaces just in case
-$tx_address =~ s/^\s+//;
-$tx_address =~ s/\s+$//;
-
-#get server time
-my $timecode = gettimeofday;
-
-#execute only if file exists
-if (@files[$int] ne ''){
-#prep mysql statement to see if this person exists already
-my $sql = "update employee set paid_this_sgement='yes', 
-employee_pay_history='$tx_address' 
-where company_wallet_id='@files[$int]' 
-and employee_pay_frequency='$pay_frequency' 
-and employee_active_status='yes' and employee_token_id='erg';";
-
-#more outputs for testing when running from terminal
-print "\n\n $tx_address  \n\n";
-print "\n\n @files[$int]  \n\n";
-print "\n\n $pay_frequency  \n\n";
+  #feedback when run in the console
+  print "(@files[$int] \n\n";
 
 
-#prepare the query
-my $sth = $dbh->prepare($sql);
+  #wipe everything after the _ in the filename, this gets us the pure wallet number of file
+  @files[$int] =~ s/_.*//s;
 
-#execute the query
-$sth->execute();
-}
+  #remove trailing and pre spaces
+  @files[$int] =~ s/^\s+//;
+  @files[$int] =~ s/\s+$//;
 
-#step up the int for the loop
-$int = $int + 1;
+  #lol, i should rename this, getting the var ito another var
+  my $tessst = @files[$int];
 
-#delete the file
-unlink($tessst)
+  #don't need to do this again, can remove
+  $tessst =~ s/^\s+//;
+  $tessst =~ s/\s+$//;
+
+  #prob can just declare this var above and remove the end from it instead of this far down
+  #this is where the TX will now be since the python script wrote it here
+  # DON'T ask me why the python script won't do a proper STDOUT like the create wallet python did, this is the only way i an get the tx for now
+  $tessst = "/usr/lib/cgi-bin/tosipayroll/erg_$tessst";
+
+  #this gets the tx from the file
+  open $fh, '<', "$tessst" or die "Can't open file $!";
+  $tx_address = do { local $/; <$fh> };
+
+  #clean up before and after spaces just in case
+  $tx_address =~ s/^\s+//;
+  $tx_address =~ s/\s+$//;
+
+  #get server time
+  my $timecode = gettimeofday;
+
+  #execute only if file exists
+  if (@files[$int] ne ''){
+    #prep mysql statement to see if this person exists already
+    my $sql = "update employee set paid_this_sgement='yes', employee_pay_history='$tx_address' where company_wallet_id='@files[$int]' and employee_pay_frequency='$pay_frequency' and employee_active_status='yes' and employee_token_id='erg';";
+
+    #more outputs for testing when running from terminal
+    print "\n\n $tx_address  \n\n";
+    print "\n\n @files[$int]  \n\n";
+    print "\n\n $pay_frequency  \n\n";
+
+
+    #prepare the query
+    my $sth = $dbh->prepare($sql);
+
+    #execute the query
+    $sth->execute();
+  }
+
+  #step up the int for the loop
+  $int = $int + 1;
+
+  #delete the file
+  unlink($tessst)
 }
 
 #bye!!!
